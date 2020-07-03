c99c6862303c861472f31c7bf58287634a47666c
keyspace <keyspaceksp@gmail.com>
2020-07-03 17:35:40 +0000

    Modify Hydrogen's EnergyDensity in Session instead.
    For now, not even modifying it, but leaving as in vanilla:
    trying to debug a dis-balance, it seems that the oxygen
    generator's IceToGasRatio is applied to the vanilla
    IceConsumptionPerSecond; and when the latter is changed,
    the re-calculation of output products is not made.

---
+++ Data/Scripts/NoMoreFreeEnergy/Session.cs
@@ -0,0 +1,38 @@
+﻿using Sandbox.Definitions;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Utils;
+
+namespace Keyspace.NoMoreFreeEnergy
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class NoMoreFreeEnergy_Session : MySessionComponentBase
+    {
+        //public static NoMoreFreeEnergy_Session Instance;
+
+        //public override void LoadData()
+        //{
+        //    Instance = this;
+        //}
+
+        public override void BeforeStart()
+        {
+            MyDefinitionId hydrogenId = new MyDefinitionId(typeof(MyObjectBuilder_GasProperties), "Hydrogen");
+            var definition = MyDefinitionManager.Static.GetDefinition(hydrogenId);
+            var gasDefinition = (MyGasProperties)definition;
+
+            // used in thrusters; i.e. not needed for mod purposes ATM
+            gasDefinition.EnergyDensity *= 1.0f;
+
+            var definition2 = MyDefinitionManager.Static.GetDefinition(hydrogenId);
+            var gasDefinition2 = (MyGasProperties)definition2;
+            MyLog.Default.WriteLineAndConsole($"DEBUG H2 EnergyDensity: {gasDefinition2.EnergyDensity}");
+        }
+
+        //protected override void UnloadData()
+        //{
+        //    Instance = null;
+        //}
+    }
+}
