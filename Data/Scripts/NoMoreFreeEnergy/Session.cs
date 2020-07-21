7f1bf4a319139b98dbfd92daf37e433423da4d02
keyspace <keyspaceksp@gmail.com>
2020-07-21 14:02:40 +0000

    Increase hydrogen engine power output to be higher than O2/H2 gen power consumption.
    This is needed to work around an exploit where an O2/H2
    generator powered by a single H2 engine (i.e. an
    under-powered gen) still produces the same amount of
    hydrogen per tick, effectively allowing to stockpile hydrogen
    slowly.
    
    In other words, an engine _must_ be able to power a
    generator!
    
    Vanilla:
    ENG: +500 kW;
    GEN: -100 kW.
    
    Before this change:
    ENG: +500 kW;
    GEN: - 600 kW.
    
    After this change:
    ENG: +750 kW;
    GEN: - 600 kW.

--- Data/Scripts/NoMoreFreeEnergy/Session.cs
+++ Data/Scripts/NoMoreFreeEnergy/Session.cs
@@ -13,12 +13,19 @@ namespace Keyspace.NoMoreFreeEnergy
 
         // Gas definition is used by hydrogen thrusters; i.e. not needed for mod purposes, at least not yet.
         private const float HYDROGEN_ENERGY_DENSITY_MULTIPLIER = 1.0f;
-        // Hydrogen engines produce more power from the same amount of gas.
+        // Hydrogen engines produce more power from the same amount of gas, and can provide more power overall
+        // compared to vanilla ones.
+        // The latter is needed to work around an exploit where an O2/H2 generator powered by a single H2 engine
+        // (i.e. an under-powered gen) still produces the same amount of hydrogen per tick, effectively allowing
+        // to stockpile hydrogen slowly...
         private const float HYDROGEN_ENGINE_EFFICIENCY_MULTIPLIER = 10.0f;
-        // Oxygen generators churn less ice in the same amount of time, and consume more power.
-        private const float OXYGEN_GENERATOR_SPEED_MULTIPLIER = 1 / HYDROGEN_ENGINE_EFFICIENCY_MULTIPLIER;
+        // ... In other words, an engine _must_ be able to power a generator!
+        private const float HYDROGEN_ENGINE_POWER_OUTPUT_MULTIPLIER = 1.5f;
+        // O2/H2 generators churn less ice in the same amount of time, and consume more power.
+        private const float OXYGEN_GENERATOR_SPEED_MULTIPLIER = 1.0f / HYDROGEN_ENGINE_EFFICIENCY_MULTIPLIER;
+        // Looks like the lowest this can go is 5.0f or so; any lower and hydrogen can be stockpiled.
         private const float OXYGEN_GENERATOR_POWER_CONSUMPTION_MULTIPLIER = 6.0f;
-        
+
         public override void LoadData()
         {
             //Instance = this;
@@ -27,12 +34,11 @@ namespace Keyspace.NoMoreFreeEnergy
             var definition = MyDefinitionManager.Static.GetDefinition(hydrogenId);
             var gasDefinition = (MyGasProperties)definition;
             gasDefinition.EnergyDensity *= HYDROGEN_ENERGY_DENSITY_MULTIPLIER;
+            // TODO: Modify character jetpack to have lower capacity?.. (So that flight duration is kept same.)
 
-            // TODO: Modify character jetpack?..
-            
             RebalanceHydrogenEngine(new MyDefinitionId(typeof(MyObjectBuilder_HydrogenEngine), "LargeHydrogenEngine"));
             RebalanceHydrogenEngine(new MyDefinitionId(typeof(MyObjectBuilder_HydrogenEngine), "SmallHydrogenEngine"));
-            
+
             RebalanceOxygenGenerator(new MyDefinitionId(typeof(MyObjectBuilder_OxygenGenerator), ""));
             RebalanceOxygenGenerator(new MyDefinitionId(typeof(MyObjectBuilder_OxygenGenerator), "OxygenGeneratorSmall"));
         }
@@ -41,6 +47,7 @@ namespace Keyspace.NoMoreFreeEnergy
         {
             var definition = MyDefinitionManager.Static.GetDefinition(definitionId) as MyHydrogenEngineDefinition;
             definition.FuelProductionToCapacityMultiplier *= HYDROGEN_ENGINE_EFFICIENCY_MULTIPLIER;
+            definition.MaxPowerOutput *= HYDROGEN_ENGINE_POWER_OUTPUT_MULTIPLIER;
         }
 
         internal void RebalanceOxygenGenerator(MyDefinitionId definitionId)
