b05918a90557eb3be6fdeee9f9dbb5f7a38fb0ed
keyspace <keyspaceksp@gmail.com>
2020-08-27 14:10:25 +0000

    copy_to_moddir.sh: add helper script.

---
+++ copy_to_moddir.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+cp -at ${APPDATA}/SpaceEngineers/Mods/NoMoreFreeEnergy/Data/Scripts/NoMoreFreeEnergy Data/Scripts/NoMoreFreeEnergy/*.cs
+cp -at ${APPDATA}/SpaceEngineers/Mods/NoMoreFreeEnergy CHANGELOG.md LICENSE.txt thumb.jpg WORKSHOP.bbcode
