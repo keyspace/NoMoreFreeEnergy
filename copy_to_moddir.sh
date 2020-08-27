d739db9090ab09c018f5a177dd1643df25bdc248
keyspace <keyspaceksp@gmail.com>
2020-08-27 21:03:36 +0000

    copy_to_moddir: copy README, too.

--- copy_to_moddir.sh
+++ copy_to_moddir.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cp -at ${APPDATA}/SpaceEngineers/Mods/NoMoreFreeEnergy/Data/Scripts/NoMoreFreeEnergy Data/Scripts/NoMoreFreeEnergy/*.cs
-cp -at ${APPDATA}/SpaceEngineers/Mods/NoMoreFreeEnergy CHANGELOG.md LICENSE.txt thumb.jpg WORKSHOP.bbcode
+cp -at ${APPDATA}/SpaceEngineers/Mods/NoMoreFreeEnergy CHANGELOG.md LICENSE.txt thumb.jpg README.md WORKSHOP.bbcode
